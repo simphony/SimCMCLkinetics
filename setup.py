bb539bd86b36ccca3eaaa9688168988321fe4ed0
Daniel Nurkowski <dln22.como@gmail.com>
2020-06-09 09:58:36 +0100

    master: Added setup.py file.

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+from packageinfo import VERSION, NAME, OSP_CORE_MIN, OSP_CORE_MAX
+
+# Read description
+with open('README.md', 'r') as readme:
+    README_TEXT = readme.read()
+
+# main setup configuration class
+setup(
+    name=NAME,
+    version=VERSION,
+    author='CMCL Innovations',
+    description='The sample CMCL kinetics wrapper for SimPhoNy',
+    keywords='CMCL, kinetics, simphony, cuds, sample',
+    long_description=README_TEXT,
+    install_requires=[
+        'osp-core>=' + OSP_CORE_MIN + ', <' + OSP_CORE_MAX,
+    ],
+    packages=find_packages(),
+    test_suite='tests',
+    entry_points={
+        'wrappers':
+            'wrapper = osp.wrappers.simdome:SimDomeSession'},
+)
