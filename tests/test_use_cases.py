b7f52432cc0131e4f99628e6d7f81e9d27af2e8c
Daniel Nurkowski <danieln@cmclinnovations.com>
2022-05-11 10:11:25 +0100

    add_use_case_tests: Added momic and stochastic solver carbon black uses case tests.

---
+++ tests/test_use_cases.py
@@ -0,0 +1,97 @@
+from osp.core.cuds import Cuds
+from osp.core import CMCL
+import osp.core.utils.simple_search as cuds_search
+from osp.wrappers.simcmclkinetics.carbon_black_engine import CarbonBlackEngine
+from osp.wrappers.simcmclkinetics.kinetics_session import KineticsSession
+from osp.wrappers.simcmclkinetics import KineticsEngine
+
+
+def _case_runner(case_cuds: Cuds, engine: KineticsEngine, model_flag: int) -> Cuds:
+    with KineticsSession(engine) as session:
+        session.setModelFlag(model_flag)
+        wrapper = CMCL.wrapper(session=session)
+        wrapper.add(case_cuds, rel=CMCL.HAS_PART)
+        wrapper.session.run()
+    return wrapper
+
+def test_carbon_black_momic(carbon_black_cuds: Cuds) -> None:
+    wrapper = _case_runner(
+        case_cuds= carbon_black_cuds,
+        engine= CarbonBlackEngine(),
+        model_flag= KineticsSession.CB_MOMIC)
+
+    # Find all the output CUDS
+    mean_part_size = cuds_search.find_cuds_objects_by_oclass(
+        oclass = CMCL.MEAN_PARTICLE_SIZE,
+        root = wrapper,
+        rel=None
+    )
+    part_num_dens = cuds_search.find_cuds_objects_by_oclass(
+        oclass = CMCL.PARTICLE_NUMBER_DENSITY,
+        root = wrapper,
+        rel=None
+    )
+    part_vol_frac = cuds_search.find_cuds_objects_by_oclass(
+        oclass = CMCL.PARTICLE_VOLUME_FRACTION,
+        root = wrapper,
+        rel=None
+    )
+
+    # check all the output CUDS
+    assert len(mean_part_size) == 1
+    assert mean_part_size[0].value != 0.0
+
+    assert len(part_num_dens) == 1
+    assert part_num_dens[0].value != 0.0
+
+    assert len(part_vol_frac) == 1
+    assert part_vol_frac[0].value != 0.0
+
+
+def test_carbon_black_stochastoc(carbon_black_cuds: Cuds) -> None:
+    wrapper = _case_runner(
+        case_cuds= carbon_black_cuds,
+        engine= CarbonBlackEngine(),
+        model_flag= KineticsSession.CB_STOCHASIC)
+
+    # Find all the output CUDS
+    part_num_dens = cuds_search.find_cuds_objects_by_oclass(
+        oclass = CMCL.PARTICLE_NUMBER_DENSITIES,
+        root = wrapper,
+        rel=None
+    )
+    part_num_size_classes = cuds_search.find_cuds_objects_by_oclass(
+        oclass = CMCL.PARTICLE_SIZE_CLASSES,
+        root = wrapper,
+        rel=None
+    )
+    prim_num_dens = cuds_search.find_cuds_objects_by_oclass(
+        oclass = CMCL.PRIMARY_PARTICLE_NUMBER_DENSITIES,
+        root = wrapper,
+        rel=None
+    )
+    prim_num_size_classes = cuds_search.find_cuds_objects_by_oclass(
+        oclass = CMCL.PRIMARY_PARTICLE_SIZE_CLASSES,
+        root = wrapper,
+        rel=None
+    )
+    part_fract_dim = cuds_search.find_cuds_objects_by_oclass(
+        oclass = CMCL.PARTICLE_MEAN_FRACTAL_DIMENSION,
+        root = wrapper,
+        rel=None
+    )
+    # check all the output CUDS
+    assert len(part_num_dens) == 1
+    assert part_num_dens[0].value_string != ""
+
+    assert len(part_num_size_classes) == 1
+    assert part_num_size_classes[0].value_string != ""
+
+    assert len(prim_num_dens) == 1
+    assert prim_num_dens[0].value_string != ""
+
+    assert len(prim_num_size_classes) == 1
+    assert prim_num_size_classes[0].value_string != ""
+
+    assert len(part_fract_dim) == 1
+    assert part_fract_dim[0].value != 0.0
