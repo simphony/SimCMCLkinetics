18700ea2e3c3e29f8cc029f4f8533e02d11c504b
Daniel Nurkowski <danieln@cmclinnovations.com>
2022-05-11 16:30:32 +0100

    add_use_case_tests: Moved the logging configuration to the app module.

--- tests/mock_agent/__init__.py
+++ tests/mock_agent/__init__.py
@@ -3,19 +3,6 @@
 from flask import Flask
 from mock_agent.routes.public import kinetics_agent_bp
 from mock_agent.routes.admin import admin_api_bp
-import os
-import logging
-import logging.config
-
-THIS_DIR = os.path.dirname(os.path.abspath(__file__))
-
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s %(message)s',
-                    handlers=[logging.FileHandler(os.path.join(THIS_DIR,"mock_agent.log"),'w'),
-                              logging.StreamHandler()])
-logging.getLogger("werkzeug").setLevel(logging.ERROR)
-logger = logging.getLogger(__name__)
-
 
 def create_app(config=None):
     """Create and configure an instance of the Flask application."""
