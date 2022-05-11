1f2da3b9451d0d78b45b56f2cb1e4f9121bd60d0
Daniel Nurkowski <danieln@cmclinnovations.com>
2022-05-11 16:54:56 +0100

    add_use_case_tests: Added tests for the kinetics mock agent.

---
+++ tests/test_mock_agent/conftest.py
@@ -0,0 +1,59 @@
+from osp.wrappers.simcmclkinetics import AgentBridge
+import pytest
+import json
+
+@pytest.fixture()
+def momic_inputs() -> str:
+    return json.dumps(
+            {
+                "Case": "CarbonBlack_MoMICSolver",
+                "Inputs": {
+                    "$INP_LENGTH_VALUE": "1.0",
+                    "$INP_LENGTH_UNIT": "m",
+                    "$INP_CROSS_SECTION_VALUE": "0.000201062",
+                    "$INP_CROSS_SECTION_UNIT": "m^2",
+                    "$INP_MASS_FLOW_RATE_VALUE": "5.82383E-05",
+                    "$INP_MASS_FLOW_RATE_UNIT": "kg/s",
+                    "$INP_PRESSURE_VALUE": "1.0",
+                    "$INP_PRESSURE_UNIT": "atm",
+                    "$INP_MIX_COMP_UNIT": "mole fraction",
+                    "$INP_MIX_COMP_C6H6_FRACTION": "0.001",
+                    "$INP_MIX_COMP_C2H2_FRACTION": "0.03",
+                    "$INP_MIX_COMP_N2_FRACTION": "0.969"
+                },
+                "Outputs":[
+                    "$OUT_MEAN_PART_DIAMETER",
+                    "$OUT_PART_NUMBER_DENS",
+                    "$OUT_PART_VOLFRAC"
+                ]
+            }
+        )
+
+@pytest.fixture()
+def stoch_inputs() -> str:
+    return json.dumps(
+            {
+                "Case": "CarbonBlack_StochasticSolver",
+                "Inputs": {
+                    "$INP_LENGTH_VALUE": "1.0",
+                    "$INP_LENGTH_UNIT": "m",
+                    "$INP_CROSS_SECTION_VALUE": "0.000201062",
+                    "$INP_CROSS_SECTION_UNIT": "m^2",
+                    "$INP_MASS_FLOW_RATE_VALUE": "5.82383E-05",
+                    "$INP_MASS_FLOW_RATE_UNIT": "kg/s",
+                    "$INP_PRESSURE_VALUE": "1.0",
+                    "$INP_PRESSURE_UNIT": "atm",
+                    "$INP_MIX_COMP_UNIT": "mole fraction",
+                    "$INP_MIX_COMP_C6H6_FRACTION": "0.001",
+                    "$INP_MIX_COMP_C2H2_FRACTION": "0.03",
+                    "$INP_MIX_COMP_N2_FRACTION": "0.969"
+                },
+                "Outputs":[
+                    "$OUT_PART_SIZE_DISTR_Y",
+                    "$OUT_PART_SIZE_DISTR_X",
+                    "$OUT_PRIM_SIZE_DISTR_Y",
+                    "$OUT_PRIM_SIZE_DISTR_X",
+                    "$OUT_PART_AVG_FRACT_DIM"
+                ]
+            }
+    )
---
+++ tests/test_mock_agent/test_mock_agent.py
@@ -0,0 +1,29 @@
+import mock_agent
+import pytest
+import json
+import urllib.parse
+
+@pytest.mark.parametrize(
+    "inputs_id",
+    [
+        ("momic_inputs"),
+        ("stoch_inputs"),
+    ],
+)
+def test_runSimulation(inputs_id: str, request, flask_client) -> None:
+    inputs = request.getfixturevalue(inputs_id)
+    case_inputs_parsed = urllib.parse.quote(inputs)
+    response = flask_client.get(f"/request?query={case_inputs_parsed}")
+
+    assert response.status_code == 200
+
+    # Parse into JSON
+    returnedJSON = json.loads(response.text)
+    # Get the generated job ID from the JSON
+    jobId_parsed = urllib.parse.quote(json.dumps({"jobId": returnedJSON["jobId"]}))
+
+    # Check if results are successfully generated
+    results = flask_client.get(f"/output/request?query={jobId_parsed}")
+
+    assert results.status_code == 200
+
