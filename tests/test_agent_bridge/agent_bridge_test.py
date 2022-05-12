53adb7863acf2f90d6454f418d18d71fc68d0dcb
Daniel Nurkowski <danieln@cmclinnovations.com>
2022-05-12 11:45:07 +0100

    add_use_case_tests: Extended mock agent tests.

--- tests/test_agent_bridge/agent_bridge_test.py
+++ tests/test_agent_bridge/agent_bridge_test.py
@@ -1,4 +1,22 @@
 from osp.wrappers.simcmclkinetics import AgentBridge
+import pytest
+import json
+
+def _mocked_request_get(url):
+    class MockResponse:
+        def __init__(self, json_data, status_code):
+            self.json_data = json_data
+            self.text = json.dumps(json_data)
+            self.status_code = status_code
+
+        def json(self):
+            return self.json_data
+
+    if 'output' in url:
+        return MockResponse({"key1": "value1"}, 200)
+    else:
+        return MockResponse({"jobId": "testId"}, 200)
+
 
 def test_encodeURL(agent_bridge: AgentBridge):
     """Tests encoding of simple JSON string
@@ -48,12 +66,32 @@ def test_buildOutputURL(agent_bridge: AgentBridge):
     assert url == expectedURL
 
 
-def test_runJob(agent_bridge: AgentBridge):
+def test_runJob(agent_bridge: AgentBridge, mocker):
     """Tests if a job can be run via HTTP requests using the runJob() function of the AgentBridge class.
     """
-    agent_bridge.jobID = None
+    mocker.patch("osp.wrappers.simcmclkinetics.agent_bridge.requests.get",
+        side_effect=_mocked_request_get)
 
     # Try to run the job
     result = agent_bridge.runJob('{}')
 
     assert result is not None
+
+
+@pytest.mark.parametrize(
+    "jsonString, result",
+    [
+        ("", False),
+        ('{"key": "value"}', True)
+    ]
+)
+def test_submitJob(jsonString: str, result: bool, agent_bridge: AgentBridge, mocker):
+    """Tests if a job can be run via HTTP requests using the runJob() function of the AgentBridge class.
+    """
+    mocker.patch("osp.wrappers.simcmclkinetics.agent_bridge.requests.get",
+        side_effect=_mocked_request_get)
+
+    # Try to run the job
+    result = agent_bridge.submitJob(jsonString)
+
+    assert result == result
--- tests/test_mock_agent/test_mock_agent.py
+++ tests/test_mock_agent/test_mock_agent.py
@@ -1,28 +1,62 @@
 import pytest
 import json
-import urllib.parse
+from typing  import Dict
 
-@pytest.mark.parametrize(
-    "inputs_id",
-    [
-        ("momic_inputs"),
-        ("stoch_inputs"),
-    ],
-)
-def test_runSimulation(inputs_id: str, request, flask_client) -> None:
-    inputs = request.getfixturevalue(inputs_id)
-    case_inputs_parsed = urllib.parse.quote(inputs)
-    response = flask_client.get(f"/request?query={case_inputs_parsed}")
 
+def test_admin(flask_client) -> None:
+    response = flask_client.get(f"/admin/")
     assert response.status_code == 200
 
-    # Parse into JSON
-    returnedJSON = json.loads(response.text)
-    # Get the generated job ID from the JSON
-    jobId_parsed = urllib.parse.quote(json.dumps({"jobId": returnedJSON["jobId"]}))
 
-    # Check if results are successfully generated
-    results = flask_client.get(f"/output/request?query={jobId_parsed}")
+@pytest.mark.parametrize(
+    "query, out_msg, status_code",
+    [
+        (
+            {},
+            "Incorrect inputs query.",
+            422
+        ),
+        (
+            {"Case": 'A_Case', "Inputs": {}, "Outputs": []},
+            "Requested Case is not supported.",
+            422
+        ),
+        (
+            {"Case": 'EAT_TWC', "Inputs": {}, "Outputs": []},
+            "Invalid Case Inputs/Ouptuts specification.",
+            422
+        ),
+    ],
+)
+def test_runSimulation(query: Dict, out_msg: str, status_code: int, flask_client) -> None:
+    response = flask_client.get(f"/request?query={json.dumps(query)}")
 
-    assert results.status_code == 200
+    assert response.text == out_msg
+    assert response.status_code == status_code
 
+
+@pytest.mark.parametrize(
+    "query, out_msg, status_code",
+    [
+        (
+            {},
+            "Incorrect get output query.",
+            422
+        ),
+        (
+            {"jobId": "testId", "testFiled": "testValue"},
+            "Incorrect get output query.",
+            422
+        ),
+        (
+            {"jobId": "testId"},
+            f"The requested job: testId does not exist.",
+            422
+        ),
+    ],
+)
+def test_getOutputs(query: Dict, out_msg: str, status_code: int, flask_client) -> None:
+    response = flask_client.get(f"/output/request?query={json.dumps(query)}")
+
+    assert response.text == out_msg
+    assert response.status_code == status_code
