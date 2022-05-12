0e8beba1f7c1e659f6ecfff7aae1560f96047509
Daniel Nurkowski <danieln@cmclinnovations.com>
2022-05-12 11:47:49 +0100

    add_use_case_tests: Simplified the agent_bridge fixture.

--- tests/test_agent_bridge/conftest.py
+++ tests/test_agent_bridge/conftest.py
@@ -4,8 +4,5 @@ import pytest
 @pytest.fixture()
 def agent_bridge() -> AgentBridge:
     agent_bridge = AgentBridge()
-    # mock the following functions
-    agent_bridge.submitJob = lambda jsonString: True
-    agent_bridge.requestOutputs = lambda : {}
     agent_bridge.POLL_INTERVAL = 0 # type: ignore
     return agent_bridge
